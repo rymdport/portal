bb3b7443bf142d4ba192070f2eb828ef1756b8ab
Jacob <jacalz@tutanota.com>
2023-11-18 23:46:58 +0100

    Implement SaveFiles as well

--- filechooser/save.go
+++ filechooser/save.go
@@ -5,14 +5,14 @@ import (
 	"github.com/rymdport/portal"
 )
 
-// SaveOptions contains the options for how files are to be selected.
-type SaveOptions struct {
+// SaveSingleOptions contains the options for how a file is saved.
+type SaveSingleOptions struct {
 	Modal bool
 }
 
 // SaveFile opens a filechooser for selecting where to save a file.
 // The chooser will use the supplied title as it's name.
-func SaveFile(title string, options *SaveOptions) error {
+func SaveFile(title string, options *SaveSingleOptions) error {
 	conn, err := dbus.SessionBus() // Shared connection, don't close.
 	if err != nil {
 		return err
@@ -25,5 +25,28 @@ func SaveFile(title string, options *SaveOptions) error {
 
 	obj := conn.Object(portal.ObjectName, portal.ObjectPath)
 	call := obj.Call(fileChooserCallName+".SaveFile", 0, parentWindow, title, data)
+	return call.Err
+}
+
+// SaveMultipleOptions contains the options for how files are saved.
+type SaveMultipleOptions struct {
+	Modal bool
+}
+
+// SaveFiles opens a filechooser for selecting where to save one or more files.
+// The chooser will use the supplied title as it's name.
+func SaveFiles(title string, options *SaveMultipleOptions) error {
+	conn, err := dbus.SessionBus() // Shared connection, don't close.
+	if err != nil {
+		return err
+	}
+
+	parentWindow := ""
+	data := map[string]dbus.Variant{
+		"modal": dbus.MakeVariant(options.Modal),
+	}
+
+	obj := conn.Object(portal.ObjectName, portal.ObjectPath)
+	call := obj.Call(fileChooserCallName+".SaveFiles", 0, parentWindow, title, data)
 	return call.Err
 }
