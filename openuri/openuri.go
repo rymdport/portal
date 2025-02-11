6408232ca4d60121acc9b393c1c804e4b3af63bc
Jacalz <jacalz@tutanota.com>
2025-02-11 14:32:33 +0100

    Fix spelling error in option field

--- openuri/openuri.go
+++ openuri/openuri.go
@@ -16,7 +16,7 @@ const (
 // OpenURIOptions holds optional settings that can be passed to the OpenURI call.
 type OpenURIOptions struct {
 	HandleToken string // A string that will be used as the last element of the handle. Must be a valid object path element.
-	Writeable   bool   // Whether to allow the chosen application to write to the file. This key only takes effect the uri points to a local file that is exported in the document portal, and the chosen application is sandboxed itself.
+	Writable    bool   // Whether to allow the chosen application to write to the file. This key only takes effect the uri points to a local file that is exported in the document portal, and the chosen application is sandboxed itself.
 	Ask         bool   // Whether to ask the user to choose an app. If this is not passed, or false, the portal may use a default or pick the last choice.
 }
 
@@ -31,7 +31,7 @@ func OpenURI(parentWindow, uri string, options *OpenURIOptions) error {
 
 	if options != nil {
 		data = map[string]dbus.Variant{
-			"writable": convert.FromBool(options.Writeable),
+			"writable": convert.FromBool(options.Writable),
 			"ask":      convert.FromBool(options.Ask),
 		}
 
