052635d3ac9faafab039e60694f56f1679fa52e3
Jacalz <jacalz@tutanota.com>
2025-02-11 14:22:07 +0100

    Add some helper types around trashcan

--- trash/trashfile.go
+++ trash/trashfile.go
@@ -5,8 +5,19 @@ import (
 	"github.com/rymdport/portal/internal/apis"
 )
 
+// TrashResult is the status for sending a file to the trashcan.
+type TrashResult = uint8
+
+const (
+	// TrashingFailed indicates that the file could not be sent to the trashcan.
+	TrashingFailed TrashResult = 0
+
+	// TrashinSucceeded indicates that the file was sent to the trashcan.
+	TrashingSucceeded TrashResult = 1
+)
+
 // TrashFile sends a file to the trashcan. Applications are allowed to trash a file if they can open it in r/w mode.
-func TrashFile(fd uintptr) (uint8, error) {
+func TrashFile(fd uintptr) (TrashResult, error) {
 	conn, err := dbus.SessionBus() // Shared connection, don't close.
 	if err != nil {
 		return 0, err
@@ -17,13 +28,13 @@ func TrashFile(fd uintptr) (uint8, error) {
 	obj := conn.Object(apis.ObjectName, apis.ObjectPath)
 	call := obj.Call(trashCallName, 0, dbus.UnixFDIndex(fd), data)
 	if call.Err != nil {
-		return 0, call.Err
+		return TrashingFailed, call.Err
 	}
 
 	var result uint8
 	err = call.Store(&result)
 	if err != nil {
-		return 0, err
+		return TrashingFailed, err
 	}
 
 	return result, nil
