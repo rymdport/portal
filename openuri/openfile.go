2928a8c3f4fc36e994c1814e8ef5ded4a7e4d732
Jacalz <jacalz@tutanota.com>
2025-02-11 14:34:18 +0100

    Add options to OpenDir and OpenFIle

--- openuri/openfile.go
+++ openuri/openfile.go
@@ -3,18 +3,37 @@ package openuri
 import (
 	"github.com/godbus/dbus/v5"
 	"github.com/rymdport/portal/internal/apis"
+	"github.com/rymdport/portal/internal/convert"
 )
 
 const openFileCallName = openURIBaseName + ".OpenFile"
 
+// OpenFileOptions holds optional settings that can be passed to the OpenFile call.
+type OpenFileOptions struct {
+	HandleToken string // A string that will be used as the last element of the handle. Must be a valid object path element.
+	Writable    bool   // Whether to allow the chosen application to write to the file. This key only takes effect the uri points to a local file that is exported in the document portal, and the chosen application is sandboxed itself.
+	Ask         bool   // Whether to ask the user to choose an app. If this is not passed, or false, the portal may use a default or pick the last choice.
+}
+
 // OpenFile asks to open a local file.
-func OpenFile(parentWindow string, fd uintptr) error {
+func OpenFile(parentWindow string, fd uintptr, options *OpenFileOptions) error {
 	conn, err := dbus.SessionBus() // Shared connection, don't close.
 	if err != nil {
 		return err
 	}
 
 	data := map[string]dbus.Variant{}
+
+	if options != nil {
+		data = map[string]dbus.Variant{
+			"writable": convert.FromBool(options.Writable),
+			"ask":      convert.FromBool(options.Ask),
+		}
+
+		if options.HandleToken != "" {
+			data["handle_token"] = convert.FromString(options.HandleToken)
+		}
+	}
 
 	obj := conn.Object(apis.ObjectName, apis.ObjectPath)
 	call := obj.Call(openFileCallName, 0, parentWindow, dbus.UnixFD(fd), data)
