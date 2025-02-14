7d9138245894ad54af3a63616cabf4e3076320ab
Jacalz <jacalz@tutanota.com>
2025-02-14 10:37:16 +0100

    Add a function for formatting the X11 window handle
    Fixes #14

--- portal.go
+++ portal.go
@@ -1,6 +1,15 @@
 package portal
 
-import "errors"
+import (
+	"errors"
+	"strconv"
+)
 
 // ErrunexpectedResonse is returned when the received dbus data was in an unexpected format.
 var ErrUnexpectedResponse = errors.New("unexpected response from dbus")
+
+// FormatX11WindowHandle takes an X11 window handle and formats it
+// in the string format that is required for the portal APIs.
+func FormatX11WindowHandle(handle uintptr) string {
+	return "x11:" + strconv.FormatUint(uint64(handle), 16)
+}
