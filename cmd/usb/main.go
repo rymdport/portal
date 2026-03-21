656c38fa4aef0ed882e1a680f55b66e8ba54f1e6
Felicitas Pojtinger <felicitas@pojtinger.com>
2026-03-20 23:32:15 -0700

    Add `acquire` command to test CLI
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- cmd/usb/main.go
+++ cmd/usb/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"maps"
 	"os"
 
@@ -9,28 +10,73 @@ import (
 )
 
 func main() {
-	devices, err := usb.EnumerateDevices()
-	if err != nil {
-		panic(err)
-	}
+	deviceID := flag.String("device", "", "device ID")
+	writable := flag.Bool("writable", false, "acquire device in read-write mode")
+
+	flag.Parse()
 
 	enc := json.NewEncoder(os.Stdout)
-	for _, dev := range devices {
-		if err := enc.Encode(map[string]any{
-			"ID":         dev.ID,
-			"Parent":     dev.Parent,
-			"Readable":   dev.Readable,
-			"Writable":   dev.Writable,
-			"DeviceFile": dev.DeviceFile,
-			"Properties": maps.Collect(func(yield func(string, any) bool) {
-				for k, v := range dev.Properties {
-					if !yield(k, v.Value()) {
-						return
-					}
-				}
-			}),
-		}); err != nil {
+
+	switch flag.Arg(0) {
+	case "enumerate":
+		devices, err := usb.EnumerateDevices()
+		if err != nil {
 			panic(err)
 		}
+
+		for _, dev := range devices {
+			if err := enc.Encode(map[string]any{
+				"ID":         dev.ID,
+				"Parent":     dev.Parent,
+				"Readable":   dev.Readable,
+				"Writable":   dev.Writable,
+				"DeviceFile": dev.DeviceFile,
+				"Properties": maps.Collect(func(yield func(string, any) bool) {
+					for k, v := range dev.Properties {
+						if !yield(k, v.Value()) {
+							return
+						}
+					}
+				}),
+			}); err != nil {
+				panic(err)
+			}
+		}
+
+	case "acquire":
+		handle, err := usb.AcquireDevices("", []usb.AcquireDeviceOptions{{ID: *deviceID, Writable: *writable}})
+		if err != nil {
+			panic(err)
+		}
+
+		result, err := usb.FinishAcquireDevices(handle)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, r := range result.Results {
+			fd := -1
+			if r.File != nil {
+				fd = int(r.File.Fd())
+			}
+
+			if err := enc.Encode(map[string]any{
+				"ID":       r.ID,
+				"Success":  r.Success,
+				"Fd":       fd,
+				"Error":    r.Error,
+				"Finished": result.Finished,
+			}); err != nil {
+				panic(err)
+			}
+		}
+
+	case "release":
+		if err := usb.ReleaseDevices([]string{*deviceID}); err != nil {
+			panic(err)
+		}
+
+	default:
+		panic("unknown command: " + flag.Arg(0) + " (expected: enumerate, acquire, release)")
 	}
 }
