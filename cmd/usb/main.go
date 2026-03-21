2ea868aff9a6bab2e20d8e02a14ff523c7a61d8e
Felicitas Pojtinger <felicitas@pojtinger.com>
2026-03-21 00:12:01 -0700

    Decompose USB command to fix issues with linter
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- cmd/usb/main.go
+++ cmd/usb/main.go
@@ -19,20 +19,105 @@ func main() {
 
 	switch flag.Arg(0) {
 	case "enumerate":
-		devices, err := usb.EnumerateDevices()
+		enumerate(enc)
+
+	case "acquire":
+		acquire(enc, *deviceID, *writable)
+
+	case "release":
+		release(*deviceID)
+
+	case "monitor":
+		monitor(enc)
+
+	default:
+		panic("unknown command: " + flag.Arg(0) + " (expected: enumerate, acquire, release, monitor)")
+	}
+}
+
+func enumerate(enc *json.Encoder) {
+	devices, err := usb.EnumerateDevices()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, dev := range devices {
+		if err := enc.Encode(map[string]any{
+			"ID":         dev.ID,
+			"Parent":     dev.Parent,
+			"Readable":   dev.Readable,
+			"Writable":   dev.Writable,
+			"DeviceFile": dev.DeviceFile,
+			"Properties": maps.Collect(func(yield func(string, any) bool) {
+				for k, v := range dev.Properties {
+					if !yield(k, v.Value()) {
+						return
+					}
+				}
+			}),
+		}); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func acquire(enc *json.Encoder, deviceID string, writable bool) {
+	handle, err := usb.AcquireDevices("", []usb.AcquireDeviceOptions{{ID: deviceID, Writable: writable}})
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := usb.FinishAcquireDevices(handle)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, r := range result.Results {
+		fd := ^uintptr(0)
+		if r.File != nil {
+			fd = r.File.Fd()
+		}
+
+		if err := enc.Encode(map[string]any{
+			"ID":       r.ID,
+			"Success":  r.Success,
+			"Fd":       fd,
+			"Error":    r.Error,
+			"Finished": result.Finished,
+		}); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func release(deviceID string) {
+	if err := usb.ReleaseDevices([]string{deviceID}); err != nil {
+		panic(err)
+	}
+}
+
+func monitor(enc *json.Encoder) {
+	sess, err := usb.CreateSession()
+	if err != nil {
+		panic(err)
+	}
+	defer sess.Close()
+
+	done := make(chan struct{})
+	sess.SetOnClosed(func(err error) {
 		if err != nil {
 			panic(err)
 		}
+		close(done)
+	})
 
-		for _, dev := range devices {
+	if err := sess.SetOnDeviceEvents(func(events []usb.DeviceEvent) {
+		for _, ev := range events {
 			if err := enc.Encode(map[string]any{
-				"ID":         dev.ID,
-				"Parent":     dev.Parent,
-				"Readable":   dev.Readable,
-				"Writable":   dev.Writable,
-				"DeviceFile": dev.DeviceFile,
-				"Properties": maps.Collect(func(yield func(string, any) bool) {
-					for k, v := range dev.Properties {
+				"Action": ev.Action,
+				"ID":     ev.ID,
+				"Device": maps.Collect(func(yield func(string, any) bool) {
+					for k, v := range ev.Device {
 						if !yield(k, v.Value()) {
 							return
 						}
@@ -42,78 +127,9 @@ func main() {
 				panic(err)
 			}
 		}
-
-	case "acquire":
-		handle, err := usb.AcquireDevices("", []usb.AcquireDeviceOptions{{ID: *deviceID, Writable: *writable}})
-		if err != nil {
-			panic(err)
-		}
-
-		result, err := usb.FinishAcquireDevices(handle)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, r := range result.Results {
-			fd := -1
-			if r.File != nil {
-				fd = int(r.File.Fd())
-			}
-
-			if err := enc.Encode(map[string]any{
-				"ID":       r.ID,
-				"Success":  r.Success,
-				"Fd":       fd,
-				"Error":    r.Error,
-				"Finished": result.Finished,
-			}); err != nil {
-				panic(err)
-			}
-		}
-
-	case "release":
-		if err := usb.ReleaseDevices([]string{*deviceID}); err != nil {
-			panic(err)
-		}
-
-	case "monitor":
-		sess, err := usb.CreateSession()
-		if err != nil {
-			panic(err)
-		}
-		defer sess.Close()
-
-		done := make(chan struct{})
-		sess.SetOnClosed(func(err error) {
-			if err != nil {
-				panic(err)
-			}
-			close(done)
-		})
-
-		if err := sess.SetOnDeviceEvents(func(events []usb.DeviceEvent) {
-			for _, ev := range events {
-				if err := enc.Encode(map[string]any{
-					"Action": ev.Action,
-					"ID":     ev.ID,
-					"Device": maps.Collect(func(yield func(string, any) bool) {
-						for k, v := range ev.Device {
-							if !yield(k, v.Value()) {
-								return
-							}
-						}
-					}),
-				}); err != nil {
-					panic(err)
-				}
-			}
-		}); err != nil {
-			panic(err)
-		}
-
-		<-done
-
-	default:
-		panic("unknown command: " + flag.Arg(0) + " (expected: enumerate, acquire, release, monitor)")
+	}); err != nil {
+		panic(err)
 	}
+
+	<-done
 }
